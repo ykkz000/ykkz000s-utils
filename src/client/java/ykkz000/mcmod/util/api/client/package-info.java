2db69b2fbe28fb367c4cd5a0887ce0b55d16dc47
ykkz000 <ykkz000@163.com>
2026-02-18 19:36:26 +0800

    Misc: Add Javadoc to api package.

--- src/client/java/ykkz000/mcmod/util/api/client/package-info.java
+++ src/client/java/ykkz000/mcmod/util/api/client/package-info.java
@@ -1,3 +1,8 @@
+/**
+ * This package provide {@link ykkz000.mcmod.util.api.client.UtilsClientApi} to access the client-side API.
+ *
+ * @author ykkz000
+ */
 @NullMarked
 @Environment(EnvType.CLIENT)
 package ykkz000.mcmod.util.api.client;
--- src/main/java/ykkz000/mcmod/util/api/package-info.java
+++ src/main/java/ykkz000/mcmod/util/api/package-info.java
@@ -1,3 +1,8 @@
+/**
+ * This package provide {@link ykkz000.mcmod.util.api.UtilsApi} to access the API.
+ *
+ * @author ykkz000
+ */
 @NullMarked
 package ykkz000.mcmod.util.api;
 
